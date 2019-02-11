ed0d347828c37add7ca85243413b3bbab16db3f9
Josh Millsap <22407981+MillsapCyber@users.noreply.github.com>
2019-02-11 06:51:56 -0600

    Upload

---
+++ app/index.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>Fib</title>
+  </head>
+  <body>
+    <h1> Welcome! </h1>
+    <h2> This is my simple fibonacci sequence api demo </h2>
+    <ul>
+      <?php
+      $json = file_get_contents('http://api');
+      echo $json;
+      ?>
+    </ul>
+    <p>
+	<ul><a href="https://docs.docker.com/install/linux/docker-ce/ubuntu/">install docker for ubuntu</a></ul>
+	<ul><a href="https://docs.docker.com/compose/install/">install docker-compose for ubuntu</a></ul>
+	<ul><a href="https://github.com/MillsapCyber/Docker-Fibonacci-Demo">source code</a></ul>
+
+    </p>
+  </body>
